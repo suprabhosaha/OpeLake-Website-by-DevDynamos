7d100dce7425567cdc555e37bb319d2596319dfe
suprabhosaha <135810003+suprabhosaha@users.noreply.github.com>
2024-07-31 23:04:06 +0530

    Update Message.js

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -4,7 +4,7 @@ import './Message.css';
 
 const Message = () => {
 
-    const { data: messages, isPending, error } = useFetch('https://json-server-openlake.vercel.app/message');
+    const { data: messages, isPending, error } = useFetch('https://json-server-openlake-nka0ctd3e-suprabhosahas-projects.vercel.app/message');
 
     return (
         <div className="message">
